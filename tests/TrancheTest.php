86c2e1d261848363c48aee887747f182455ad561
Peter Johnstone <peter.wdj@gmail.com>
2018-03-01 17:16:09 +0000

    Updates maximumAvailable getter name; adds test for currentlyInvested property

--- tests/TrancheTest.php
+++ tests/TrancheTest.php
@@ -9,6 +9,12 @@ class TrancheTest extends TestCase
   public function testMaximumAvailable()
   {
     $tranche = new Tranche();
-    $this->assertSame(1000, $tranche->displayMaximumAvailable());
+    $this->assertSame(1000, $tranche->getMaximumAvailable());
+  }
+
+  public function testCurrentlyInvested()
+  {
+    $tranche = new Tranche();
+    $this->assertSame(0, $tranche->getCurrentlyInvested());
   }
 }
