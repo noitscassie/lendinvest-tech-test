26d631aeeac769fed29ef40b2993306a282c7379
Peter Johnstone <peter.wdj@gmail.com>
2018-03-01 13:16:40 +0000

    Adds test for invest method

--- tests/InvestorTest.php
+++ tests/InvestorTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 include(dirname(__FILE__)."/../src/Investor.php");
+include(dirname(__FILE__)."/../src/Tranche.php");
 
 class InvestorTest extends TestCase
 {
@@ -11,5 +12,13 @@ class InvestorTest extends TestCase
     $warren = new Investor(1000);
     $cash = $warren->getCash();
     $this->assertSame(1000, $cash);
+  }
+
+  public function testInvest()
+  {
+    $warren = new Investor(1000);
+    $tranche = $this->createMock(Tranche::class);
+    $warren->invest(500, $tranche);
+    $this->assertSame(500, $warren->getCash());
   }
 }
