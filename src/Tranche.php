f9882078974dc02ade8161d1fbcd33bb95377e51
Peter Johnstone <peter.wdj@gmail.com>
2018-03-01 15:36:21 +0000

    Adds maximumAvailable property and method to return it

--- src/Tranche.php
+++ src/Tranche.php
@@ -2,5 +2,10 @@
 declare(strict_types=1);
 
 class Tranche {
+  var $maximumAvailable = 1000;
   function addFunds() {}
+
+  function displayMaximumAvailable() {
+    return $this->maximumAvailable;
+  }
 }
