9a6ffb68577a8a7e25aad576b7c964e979bfc6a9
Peter Johnstone <peter.wdj@gmail.com>
2018-03-02 13:45:42 +0000

    Extracts investment instnatiation to setUp method

--- tests/InvestmentTest.php
+++ tests/InvestmentTest.php
@@ -6,13 +6,15 @@ include(dirname(__FILE__)."/../src/Investment.php");
 
 class InvestmentTest extends TestCase
 {
+  public function setUp() {
+    $this->investment = new Investment("2015-10-03", 6);
+  }
+
   public function testGetStartDate() {
-    $investment = new Investment("2015-10-03", 6);
-    $this->assertEquals(date_create("2015-10-03"), $investment->getStartDate());
+    $this->assertEquals(date_create("2015-10-03"), $this->investment->getStartDate());
   }
 
   public function testGetInterestRate() {
-    $investment = new Investment("2015-10-03", 6);
-    $this->assertEquals(6, $investment->getInterestRate());
+    $this->assertEquals(6, $this->investment->getInterestRate());
   }
 }
