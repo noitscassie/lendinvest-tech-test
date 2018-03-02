d5939d6f0947919e111e0dbbe4385434ed6aa209
Peter Johnstone <peter.wdj@gmail.com>
2018-03-02 12:40:55 +0000

    Adds intrestRate property and getter method

--- src/Investment.php
+++ src/Investment.php
@@ -3,12 +3,18 @@ declare(strict_types=1);
 
 class Investment {
   var $startDate;
+  var $interestRate;
 
-  function __construct($startDate) {
+  function __construct($startDate, $interestRate) {
     $this->startDate = date_create($startDate);
+    $this->interestRate = $interestRate;
   }
 
   function getStartDate() {
     return $this->startDate;
+  }
+
+  function getInterestRate() {
+    return $this->interestRate;
   }
 }
