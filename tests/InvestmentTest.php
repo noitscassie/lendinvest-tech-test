56f4e3a02d49386ef8be49421f80459fd6b097b8
Peter Johnstone <peter.wdj@gmail.com>
2018-03-02 12:40:35 +0000

    Adds test for interestRate property getter

--- tests/InvestmentTest.php
+++ tests/InvestmentTest.php
@@ -7,7 +7,12 @@ include(dirname(__FILE__)."/../src/Investment.php");
 class InvestmentTest extends TestCase
 {
   public function testGetStartDate() {
-    $investment = new Investment("2015-10-03");
+    $investment = new Investment("2015-10-03", 6);
     $this->assertEquals(date_create("2015-10-03"), $investment->getStartDate());
+  }
+
+  public function testGetInterestRate() {
+    $investment = new Investment("2015-10-03", 6);
+    $this->assertEquals(6, $investment->getInterestRate());
   }
 }
