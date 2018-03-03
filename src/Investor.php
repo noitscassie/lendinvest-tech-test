cbd5bd4c427f34458f913225f6a19872b4c5fd1e
Peter Johnstone <peter.wdj@gmail.com>
2018-03-03 13:13:42 +0000

    #invest method creates new Investment object and stores it to investment variable

--- src/Investor.php
+++ src/Investor.php
@@ -17,8 +17,9 @@ class Investor {
     return $this->investment;
   }
 
-  function invest($amount, $tranche) {
+  function invest($amount, $tranche, $date) {
     $tranche->addFunds($amount);
+    $investment = $this->createInvestment($date, $tranche->getInterestRate(), $amount);
     $this->decreaseCash($amount);
     return "ok";
   }
@@ -26,8 +27,8 @@ class Investor {
   private function decreaseCash($amount) {
     $this->cash = $this->cash - $amount;
   }
-  //
-  // private function createInvestment($date, $interestRate, $amount) {
-  //   return new Investment($date, $interestRate, $amount);
-  // }
+
+  private function createInvestment($date, $interestRate, $amount) {
+    $this->investment = new Investment($date, $interestRate, $amount);
+  }
 }
